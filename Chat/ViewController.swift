7fc702c6dc529832aa145a38ffa8b7c11addbfb9
memo figueredo <memof80@gmail.com>
2019-01-29 09:22:05 -0500

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Memo Figueredo on 1/29/19.
+//  Copyright © 2019 Memo Figueredo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
