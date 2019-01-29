69c6f6d3ae8a26994c4e028dee5633b15542feaa
memo figueredo <memof80@gmail.com>
2019-01-29 11:57:10 -0500

    se agregaron los conectores y acciones así como una extensión al viewController permitiendo delegar una funcionalidad adicional a uitexfield

--- Chat/ViewController.swift
+++ Chat/ViewController.swift
@@ -10,11 +10,81 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    //conectores
+    
+    @IBOutlet weak var respuestaJavier: UILabel!
+    
+    @IBOutlet weak var insertarPregunta: UITextField!
+    
+    
+    @IBOutlet weak var labelPreguntar: UIButton!
+    
+    let myQuestion = Respuesta()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+  
+    
+    // funciones
+    
+    func respondemeMisPreguntas(_ question: String) {
+        
+        let respuesta = myQuestion.respondeme(question: question)
+        
+        displayRespuestaTextoScreen(respuesta)
+        
+        insertarPregunta.placeholder = "haz otra pregunta"
+        
+        insertarPregunta.text = nil
+    }
+    
+    
+   
+    
+   
+    //acciones
 
-
+    @IBAction func agregarPregunta(_ sender: UITextField) {
+        
+        
+    }
+    
+    
+    
+    @IBAction func pregunta(_ sender: Any) {
+        
+        guard insertarPregunta.text != nil else {
+            return
+        }
+        
+       insertarPregunta.resignFirstResponder()
+        
+    }
+    
+   //funcion 2
+    func displayRespuestaTextoScreen(_ answer: String) {
+        
+        respuestaJavier.text = answer
+    }
+ 
 }
 
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        insertarPregunta.resignFirstResponder()
+        return false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let texto = insertarPregunta.text else {
+            return
+        }
+        
+    respondemeMisPreguntas(texto)
+    }
+}
